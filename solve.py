c02b08040650cca1be83e12d0f06ece993d8cbed
HotS1de <hots1de@mail.ru>
2026-04-23 23:12:07 +0300

    мат модель + актив решатель на модель

--- solve.py
+++ solve.py
@@ -38,29 +38,38 @@ def cij_matrix(points_list):
                 cij[i][j] = dist
     return cij
 
-def model(points_list, C, K):
-    """
-        Решает задачу размещения магистральных узлов.
-
-        Вход:
-            points_list: список координат [[x1,y1], [x2,y2], ...]
-            C: стоимость одного core-узла
-            K: максимальное количество подключений к одному core-узлу
-
-        Выход:
-            словарь с ключами: core_nodes, connections, total_cost
-        """
+def model_solver(points_list, C, K):
     n = len(points_list)
     cij = cij_matrix(points_list)
     model = pyo.ConcreteModel()
     model.z = pyo.Var(range(n), domain = pyo.Binary)
     model.e = pyo.Var(range(n), range(n), domain = pyo.Binary)
-    model.obj = pyo.Objective(func = C * sum(model.z[j] for j in range(n)) + sum(cij[i][j] * model.e[i,j] for i in range(n) for j in range(n)), find = pyo.minimize)
+    model.obj = pyo.Objective(expr = C * sum(model.z[j] for j in range(n)) + sum(cij[i][j] * model.e[i,j] for i in range(n) for j in range(n)), sense = pyo.minimize)
     model.constraint1 = pyo.ConstraintList()
     for i in range(n):
-        model.constraint1.add(sum(model.e[i,j] for j in range(n) == 1))
+        model.constraint1.add(sum(model.e[i,j] for j in range(n)) == 1)
     model.constraint2 = pyo.ConstraintList()
     for j in range(n):
         model.constraint2.add(sum(model.e[i, j] for i in range(n)) <= K * model.z[j])
-    solver = pyo.SolverFactory('cbc')
+    solver = pyo.SolverFactory('glpk')
     result = solver.solve(model, tee = False)
+
+    opened_magistrals = []
+    for j in range(n):
+        if pyo.value(model.z[j]) > 0.9:
+            opened_magistrals.append(j)
+
+    connections = []
+    for i in range(n):
+        for j in range(n):
+            if pyo.value(model.e[i, j]) > 0.9:
+                connections.append([i, j])
+
+    total_cost = pyo.value(model.obj)
+
+    return {
+        "opened_magistrals": opened_magistrals,
+        "connections": connections,
+        "total_cost": total_cost
+    }
+
