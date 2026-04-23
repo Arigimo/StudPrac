7b2160ceb0c151d4ee1652441424c425a537cf9d
Makar Larionenko <maklar092006@gmail.com>
2026-04-23 16:01:00 +0300

    импорт пиомо

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("hello world")
+import pyomo.environ as pyo
+import math
+
+locations = {
+    0:(0, 0),
+    1:(2,3),
+    2:(3,4),
+    3:(5,6),
+}
+
+location_list = list(locations.keys())
